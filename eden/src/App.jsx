bd08cbee769437d3c3f2143474bf564a984835e3
Philipp Elsässer <philipp-elsaesser@outlook.com>
2018-12-20 15:59:25 +0100

    added heading

--- eden/src/App.jsx
+++ eden/src/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Heading from './Container/Heading';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Heading />
       </div>
     );
   }
