7ff066e02b7a68a194324cf1694d34325f0e4c13
JN Bourrat <jn.brrt@yahoo.fr>
2018-12-20 14:53:35 +0100

    Ajout de l'attribut trivia dans le tableau de question

--- eden/src/Reducer/questionReducer.jsx
+++ eden/src/Reducer/questionReducer.jsx
@@ -4,11 +4,13 @@ export default () => {
       id: 1,
       question: 'Question text',
       answer: 'oui', 
+      trivia: 'Remplacer ce texte par une information intéressante liée au sujet de la question'
     },
     {
       id: 2,
       question: 'Question text',
       answer: 'oui', 
+      trivia: 'Remplacer ce texte par une information intéressante liée au sujet de la question'
     },
   ]
 };
