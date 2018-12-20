86643b10fc55b0b26676e8ff180d94c083e34174
JN Bourrat <jn.brrt@yahoo.fr>
2018-12-20 17:34:09 +0100

    fix boardgame canvas

--- eden/src/Components/BoardGame.jsx
+++ eden/src/Components/BoardGame.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import Heading from '../Container/Heading';
-import '../Assets/Style/boardGame.css'
+import background from '../Assets/Images/background2.png'
+
+import { Container, Row, Col} from 'reactstrap';
 
 class BoardGame extends Component {
   render() {
     return (
       <div className="App">
-        <Heading />
-        <div class="boardgame">
-        </div>
+        <Container>
+          <Row>
+            <Col sm='12'>
+              <img src={background} className="img-fluid"/>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
