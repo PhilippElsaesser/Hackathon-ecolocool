26f7882ea253829663daa4eaa0a9f2c97826a443
JN Bourrat <jn.brrt@yahoo.fr>
2018-12-20 16:42:38 +0100

    Create background boardgame

--- eden/src/Components/BoardGame.jsx
+++ eden/src/Components/BoardGame.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Heading from '../Container/Heading';
+import '../Assets/Style/boardGame.css'
 
 class BoardGame extends Component {
   render() {
     return (
       <div className="App">
         <Heading />
+        <div class="boardgame">
+        </div>
       </div>
     );
   }
