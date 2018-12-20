dc744d24d089d5fa9d3256fc789ad8e7d966ac93
JN Bourrat <jn.brrt@yahoo.fr>
2018-12-20 21:23:18 +0100

    Solved back button placement problem

--- eden/src/Container/Heading.jsx
+++ eden/src/Container/Heading.jsx
@@ -3,19 +3,16 @@ import { Container, Row, Col } from 'reactstrap';
 import { FaChevronLeft } from 'react-icons/fa';
 import { NavLink, Link } from 'react-router-dom';
 import '../Assets/Style/Heading.css'
+
 class Heading extends Component {
   render() {
     return (
       <div>
         <Row>
-          <Col sm="1">
-            <NavLink to="/"><h1 className="text-success"><FaChevronLeft /></h1></NavLink>
-          </Col>
-          <Col sm="10">
+          <NavLink to="/"><h1 className="backbutton"><FaChevronLeft /></h1></NavLink>
+          <Col sm="12">
             <h1>Eden</h1>
             <p className="p-app">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean placerat est orci, et sodales mi ultricies sagittis. Curabitur non vehicula tellus, id fermentum quam. </p>
-          </Col>
-          <Col sm="1">
           </Col>
         </Row>
         
