2d7686fe853956716d92f1172d65e7649ae2b148
Kapucine <nielsen.kapucine@gmail.com>
2018-12-20 17:34:05 +0100

    commit

--- eden/src/Components/BoardGame.jsx
+++ eden/src/Components/BoardGame.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import Heading from '../Container/Heading';
-import '../Assets/Style/boardGame.css'
+import '../Assets/Style/boardGame.css';
+import background from '../Assets/Images/background2.png';
 
 class BoardGame extends Component {
   render() {
     return (
       <div className="App">
         <Heading />
-        <div class="boardgame">
-        </div>
+        <Container>
+        <Row>
+          <Col sm="12">
+            <img src={background} className="img-fluid" alt=""/>
+          </Col>
+        </Row>
+        </Container>
       </div>
     );
   }
--- eden/src/Container/Heading.jsx
+++ eden/src/Container/Heading.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Row, Col} from 'reactstrap';
 
-
 class Heading extends Component {
   render() {
     return (
