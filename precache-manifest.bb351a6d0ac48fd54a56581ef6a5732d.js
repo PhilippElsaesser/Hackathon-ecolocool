1ef3affb5bf3ea0104d82ba2d30a43d8ee9af521
JN Bourrat <jn.brrt@yahoo.fr>
2019-01-03 17:46:29 +0100

    Updates

---
+++ precache-manifest.bb351a6d0ac48fd54a56581ef6a5732d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "a39575de53581bc75b0a",
+    "url": "/eden/static/css/main.3b627e0c.chunk.css"
+  },
+  {
+    "revision": "a39575de53581bc75b0a",
+    "url": "/eden/static/js/main.a39575de.chunk.js"
+  },
+  {
+    "revision": "d32f42b1b45a8b9c292c",
+    "url": "/eden/static/css/1.3d4d2a29.chunk.css"
+  },
+  {
+    "revision": "d32f42b1b45a8b9c292c",
+    "url": "/eden/static/js/1.d32f42b1.chunk.js"
+  },
+  {
+    "revision": "06bd0bfe43f8c7894ee4",
+    "url": "/eden/static/js/runtime~main.06bd0bfe.js"
+  },
+  {
+    "revision": "b991975bde1dd4f3edaac2ffb86eedf8",
+    "url": "/eden/static/media/background.b991975b.png"
+  },
+  {
+    "revision": "3ee4a94b3a716949b63d87828327dd3d",
+    "url": "/eden/static/media/character.3ee4a94b.png"
+  },
+  {
+    "revision": "43e075bd1af672e4ab98b028afc9a7ae",
+    "url": "/eden/static/media/paysage1.43e075bd.png"
+  },
+  {
+    "revision": "e41b7cf2c3f2c5edf915715a13f117ee",
+    "url": "/eden/static/media/paysage6.e41b7cf2.png"
+  },
+  {
+    "revision": "34d31bdda99366a6a490c86c4024de63",
+    "url": "/eden/static/media/day1.34d31bdd.png"
+  },
+  {
+    "revision": "c2041e84dc19d9d2940acd7d8b280b4e",
+    "url": "/eden/static/media/day2.c2041e84.png"
+  },
+  {
+    "revision": "27c7c794854c9aa362b6b0cc515eb5a1",
+    "url": "/eden/static/media/day3.27c7c794.png"
+  },
+  {
+    "revision": "331010c8dcfdb2d610616c8200866bd9",
+    "url": "/eden/static/media/day4.331010c8.png"
+  },
+  {
+    "revision": "8b52bc1ee18af3e8d123369940f5f25a",
+    "url": "/eden/static/media/day5.8b52bc1e.png"
+  },
+  {
+    "revision": "fd3d806c24e15f82e905af0d0728c285",
+    "url": "/eden/static/media/day6.fd3d806c.png"
+  },
+  {
+    "revision": "b640f30ada55ae679d5b2fa0d1a66f66",
+    "url": "/eden/static/media/day7.b640f30a.png"
+  },
+  {
+    "revision": "d4638a7d370f886f2c8970958bcf6224",
+    "url": "/eden/static/media/day8.d4638a7d.png"
+  },
+  {
+    "revision": "c6accc0e95d827d8ebc7f7908683352c",
+    "url": "/eden/static/media/logo.c6accc0e.png"
+  },
+  {
+    "revision": "b843fe246889cac9e365cde66b98d698",
+    "url": "/eden/index.html"
+  }
+];
